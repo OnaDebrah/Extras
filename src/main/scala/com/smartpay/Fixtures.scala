041123ca27e8f368e4bac8e4184ca2412c5c6cf5
OnaDebrah <o.debrah@stuart.com>
2021-12-18 23:53:44 +0100

    Created Fixtures file

--- src/main/scala/com/smartpay/Fixtures.scala
+++ src/main/scala/com/smartpay/Fixtures.scala
@@ -1,9 +1,23 @@
 package com.smartpay
 
-trait Kinds {
+import com.typesafe.scalalogging.LazyLogging
+import io.circe.generic.auto._
+import io.circe.jawn.decode
 
-  val list = List("sc2-micro","sc2-small","sc2-medium","sc2-std-2","sc2-std-4","sc2-std-8","sc2-std-16",
-    "sc2-std-32","sc2-himem-2","sc2-himem-4","sc2-himem-8","sc2-himem-16","sc2-himem-32","sc2-hicpu-2",
-    "sc2-hicpu-4","sc2-hicpu-8","sc2-hicpu-16","sc2-hicpu-32")
+import java.io.{BufferedWriter, File, FileWriter}
+import scala.language.postfixOps
+
+case class Prices(kind: String,
+                  price: Double,
+                  timestamp: String)
+
+
+trait Fixtures extends LazyLogging {
+
+  val priceKinds = List("sc2-micro","sc2-small","sc2-medium","sc2-std-2","sc2-std-4","sc2-std-8","sc2-std-16","sc2-std-32",
+    "sc2-himem-2","sc2-himem-4","sc2-himem-8","sc2-himem-16","sc2-himem-32","sc2-hicpu-2","sc2-hicpu-4","sc2-hicpu-8","sc2-hicpu-16","sc2-hicpu-32")
+
+
+
 
 }
